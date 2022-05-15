71efe7b9bf1977d41b38d57f0422df7d137ebbc5
kevkhv <kevkhv@yandex.ru>
2022-05-15 20:05:58 +1000

    Clear main commit

--- src/main/kotlin/Service.kt
+++ src/main/kotlin/Service.kt
@@ -4,7 +4,6 @@ class Service {
     private fun findChat(id: Int): Chat {
         return chats.firstOrNull { it.chatId == id } ?: throw ChatNotFoundException("Чат не найден")
     }
-
     private fun findMessage(cid: Int, mid: Int): Message {
         return findChat(cid).message.firstOrNull { it.messageId == mid }
             ?: throw MessageNotFoundException("Сообщение не найдено")
@@ -14,40 +13,31 @@ class Service {
         cid++
         chats.add(item.copy(chatId = cid))
         return chats.first{it.chatId == cid}
-    }     //++
+    }
 
     fun deleteChat(item: Chat): Boolean {
         chats.remove(findChat(item.chatId))
         return true
-    } //++
+    }
 
     fun getChats(): List<Chat> {
-//        if (chats.size > 0) {
-//            chats.forEach() { element ->
-//                if (element.message.isNotEmpty()) {
-//                    print("(${element.chatId})${element.createdBy.name}(${element.createdBy.userId}) -> ${element.ownerBy.name}(${element.ownerBy.userId}):   ")
-//                    element.message.forEach { print("${it.text}(${it.messageId})(${it.chatId})...") }
-//                    println()
-//                } else println("(${element.chatId})нет сообщений")
-//            }
-//        } else println("Чаты отсутствуют")
         return chats
-    }                      //++
+    }
 
     fun createMessage(item: Message) {
         chats.firstOrNull { it.chatId == item.chatId }
             ?.apply { message.add(item.copy(messageId = message.last().messageId + 1)) } ?:
             createChat(Chat(createdBy = item.sender, ownerBy = item.addressee)).apply { message.add(item.copy(chatId = chatId, messageId = 1)) }
-    }    //++
+    }
 
     fun editMessage(item: Message) {
         findMessage(item.chatId, item.messageId).apply { text = item.text }
-    }      //++
+    }
 
     fun deleteMessage(item: Message) {
         findChat(item.chatId).message.remove(findMessage(item.chatId, item.messageId))
         if (findChat(item.chatId).message.isEmpty()) deleteChat(findChat(item.chatId))
-    }    //++
+    }
 
     fun getMessage(cid: Int, lastMid: Int, count: Int): List<Message> {
         val result = mutableListOf<Message>()
@@ -58,7 +48,7 @@ class Service {
             }
         }
         return result.ifEmpty { throw MessageNotFoundException("Сообщение с ID = $lastMid не найдено") }
-    }   //++
+    }  
 
     fun getUnreadChatsCount(userId: Int): Int {
         return chats.count { elements ->
